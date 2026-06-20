9e5f0594f31e4d0b68ec21efee29f43135744289
Mobashir <mobashir49@outlook.com>
2026-06-20 21:16:16 +0530

    Completed Exercise One

--- Ex-1_basics/10.c
+++ Ex-1_basics/10.c
@@ -1,8 +1,8 @@
 // Write a program to demonstrate the use of symbolic constants using both the `#define` preprocessor directive and the `const` variable modifier, and explain their operational differences.
 #include<stdio.h>
 
-#define PI 3.14159
-const float E = 2.71828;
+#define PI 3.141592
+const float E = 2.718281;
 
 int main() {
     printf("PI (using #define): %f\n", PI);
